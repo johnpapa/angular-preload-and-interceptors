2beac802a26aef4cd7cd085872e563ed5a4194f0
John Papa <john@johnpapa.net>
2020-04-02 23:32:12 -0400

    headers ust be logged after they are stuffed

--- src/app/core/interceptors/index.ts
+++ src/app/core/interceptors/index.ts
@@ -11,14 +11,14 @@ import { ReadOnlyInterceptor } from './read-only.interceptor';
 export const httpInterceptorProviders = [
   // Log Http: Should be first-ish so it can log the Http call happening in and out (last).
   { provide: HTTP_INTERCEPTORS, useClass: LogHttpInterceptor, multi: true },
-  // Log headers: Must come after the headers are stuffed.
-  { provide: HTTP_INTERCEPTORS, useClass: LogHeadersInterceptor, multi: true },
   //ReadOnly: Do this before we add headers, get busy, or make the call.
   { provide: HTTP_INTERCEPTORS, useClass: ReadOnlyInterceptor, multi: true },
   //SSL, Auth, CSRF:Now that it has passed the readonly test, we want to stuff headers and proceed.
   { provide: HTTP_INTERCEPTORS, useClass: EnsureSSLInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: CSRFInterceptor, multi: true },
+  // Log headers: Must come after the headers are stuffed.
+  { provide: HTTP_INTERCEPTORS, useClass: LogHeadersInterceptor, multi: true },
   // Busy: Should be first so it can turn on first, and off last.
   { provide: HTTP_INTERCEPTORS, useClass: BusyInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: TransformResponseInterceptor, multi: true }
