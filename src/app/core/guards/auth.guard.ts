cb89907feeeba6f8f13350d148692638c128b7de
John Papa <john@johnpapa.net>
2023-06-10 15:28:10 -0400

    tweak to comments

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -19,10 +19,10 @@ export const authGuard = (route: ActivatedRouteSnapshot, state: RouterStateSnaps
   return false;
 };
 
-//// OLD GUARDS
+//// CLASS-IC GUARD
 
 // import { Injectable } from '@angular/core';
-// import { Route, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+// import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 // import { SessionService } from '../session.service';
 
 // @Injectable({ providedIn: 'root' })
@@ -30,18 +30,6 @@ export const authGuard = (route: ActivatedRouteSnapshot, state: RouterStateSnaps
 //   deniedMessage = '💂‍♀️ [Guard] - Auth Guard - Unauthorized access denied';
 
 //   constructor(private sessionService: SessionService, private router: Router) {}
-
-//   canLoad(route: Route) {
-//     if (this.sessionService.isLoggedIn) {
-//       console.log(`💂‍♀️ [Guard] - Auth Guard - allowed`);
-//       return true;
-//     }
-
-//     const url = `/${route.path}`;
-//     this.router.navigate(['/signin'], { queryParams: { redirectTo: url } });
-//     console.warn(this.deniedMessage);
-//     return this.sessionService.isLoggedIn;
-//   }
 
 //   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 //     if (this.sessionService.isLoggedIn) {
@@ -52,9 +40,5 @@ export const authGuard = (route: ActivatedRouteSnapshot, state: RouterStateSnaps
 //     });
 //     console.warn(this.deniedMessage);
 //     return false;
-//   }
-
-//   canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-//     return this.canActivate(route, state);
 //   }
 // }
