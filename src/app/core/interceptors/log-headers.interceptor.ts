714c5bbb6f2062ae8b3740dff85b9ea950c639c2
John Papa <john@johnpapa.net>
2024-03-19 13:36:59 -0600

    Refactor BusyInterceptor and LogHeadersInterceptor to use functional implementation

--- src/app/core/interceptors/log-headers.interceptor.ts
+++ src/app/core/interceptors/log-headers.interceptor.ts
@@ -1,4 +1,11 @@
-import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpEvent,
+  HttpInterceptorFn,
+  HttpHandlerFn,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { prefixReq } from './http-config';
@@ -6,21 +13,29 @@ import { prefixReq } from './http-config';
 @Injectable()
 export class LogHeadersInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.logRequest(req);
+    logRequest(req);
     return next.handle(req);
   }
+}
 
-  private logRequest(req: HttpRequest<any>) {
-    console.groupCollapsed(`${prefixReq} ⚽️ Headers`);
-    const headerList: {
-      key: string;
-      values: string;
-    }[] = [];
-    req.headers.keys().map((key) => {
-      headerList.push({ key, values: req.headers.getAll(key).toString() });
-    });
-    console.log(`${req.method} "${req.urlWithParams}"`);
-    console.table(headerList);
-    console.groupEnd();
-  }
+export const LogHeadersFunctionalInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) => {
+  logRequest(req);
+  return next(req);
+};
+
+function logRequest(req: HttpRequest<any>) {
+  console.groupCollapsed(`${prefixReq} ⚽️ Headers`);
+  const headerList: {
+    key: string;
+    values: string;
+  }[] = [];
+  req.headers.keys().map((key) => {
+    headerList.push({ key, values: req.headers.getAll(key).toString() });
+  });
+  console.log(`${req.method} "${req.urlWithParams}"`);
+  console.table(headerList);
+  console.groupEnd();
 }
