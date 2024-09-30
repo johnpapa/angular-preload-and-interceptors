0009cc17f63a215c75ca31d2290af231d878989f
johnpapa <john@johnpapa.net>
2024-09-30 17:59:59 -0400

    Refactor import statements in interceptors

--- src/app/core/interceptors/transform-response.interceptor.ts
+++ src/app/core/interceptors/transform-response.interceptor.ts
@@ -1,10 +1,4 @@
-import {
-  HttpInterceptor,
-  HttpHandler,
-  HttpRequest,
-  HttpEvent,
-  HttpResponse,
-} from '@angular/common/http';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
